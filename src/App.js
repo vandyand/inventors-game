fe0d07bc018a60ac207ae5ea9b77680a622d2c68
Andrew Van Dyke <vandyand@gmail.com>
2020-07-13 22:07:02 -0400

    wip: cleaned App.js and updated board style

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import React from "react";
 import Board from "./components/Board";
 
 const App = () => {
-  return (
-    <>
-      <div>chess</div>
-      <Board />
-    </>
-  );
+  return <Board />;
 };
 
 export default App;
