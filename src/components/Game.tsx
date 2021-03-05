2181fe208d2e8f59dd77f344c1bb4ec0f058a036
vandyand <vandyand@gmail.com>
2021-03-05 10:09:46 -0500

    fix: get game running with ts

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -4,22 +4,23 @@ import Board from "./Board";
 import Controls from "./Controls";
 import GameEngine from "../gameEngine/gameEngine.container";
 
-const Game = (state: any) => (
-  <div className="app">
-    <Board
-    // boards={undefined}
-    // currentGameTypeCode={undefined}
-    // gameTypes={undefined}
-    />
-    <Controls />
-    {state.currentGame.winner && `Team ${state.currentGame.winner} wins!`}
-    <br />
-    <GameEngine />
-  </div>
-);
+const Game = (state: any) => {
+  console.log(state);
+  return (
+    <div className="app">
+      <Board
+      // boards={undefined}
+      // currentGameTypeCode={undefined}
+      // gameTypes={undefined}
+      />
+      <Controls />
+      {state.currentGame.winner && `Team ${state.currentGame.winner} wins!`}
+      <br />
+      <GameEngine />
+    </div>
+  );
+};
 
-const mapStateToProps = (state: any) => ({
-  state,
-});
+const mapStateToProps = (state: any) => state;
 
-export default connect(mapStateToProps, null)(Game);
+export default connect(mapStateToProps)(Game);
