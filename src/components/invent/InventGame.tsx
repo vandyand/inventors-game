cf2282f8f4395f573bf250f548eaeb5bd9c133c8
vandyand <vandyand@gmail.com>
2021-10-22 16:30:50 -0400

    chore: navigation

--- src/components/invent/InventGame.tsx
+++ src/components/invent/InventGame.tsx
@@ -14,24 +14,27 @@ type Props = {
 
 const InventGameForm = (props: Props) => {
   return (
-    <form className="InventGame">
-      <h1>Invent a game here </h1>
-      <div className="InventGame__Board">
-        <Field
-          component={SelectBoard}
-          title="Select a board"
-          boards={props.boards}
-          name="SelectBoard"
-        />
-        <Link to="/Invent/Board">Invent a new board</Link>
-      </div>
-      <div className="InventGame__Piece">
-        <Field
-          component={SelectPieces}
-          title="Select your pieces" />
-        <Link to="/Invent/Pieces">Invent new pieces</Link>
-      </div>
-    </form>
+    <>
+      <Link to="/">Main Menu</Link>
+      <form className="InventGame">
+        <h1>Invent a game here </h1>
+        <div className="InventGame__Board">
+          <Field
+            component={SelectBoard}
+            title="Select a board"
+            boards={props.boards}
+            name="SelectBoard"
+          />
+          <Link to="/Invent/Board">Invent a new board</Link>
+        </div>
+        <div className="InventGame__Piece">
+          <Field
+            component={SelectPieces}
+            title="Select your pieces" />
+          <Link to="/Invent/Pieces">Invent new pieces</Link>
+        </div>
+      </form>
+    </>
   );
 };
 
